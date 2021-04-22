9bef1d7d5bc0a45be69bffde4db869b13b17e9de
Teller Wetzel <tellerwetzel@yahoo.com>
2021-04-22 17:21:39 -0600

    start of java fetch command

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,12 +3,24 @@ var date = document.querySelector("#display-time");
 var rightNow = moment().format("dddd, MMMM Do, hh:mm a");
 date.textContent = rightNow;
 
+
+function myFunction() {
+  var searchTerm = document.querySelector("#searchTerm").value;
+  
 fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=salt lake city&units=imperial&appid=27bc004fc8746779f50a74c093ff9a93"
+    "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm  + "&units=imperial&appid=27bc004fc8746779f50a74c093ff9a93"
   )
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
+     $("#responseCity").empty();
+     for (var i = 0; i < data.main.length; i++){
+       var temp
+       var wind
+       var humidity
+       var uv
+     }
       console.log(data);
     });
+  }
