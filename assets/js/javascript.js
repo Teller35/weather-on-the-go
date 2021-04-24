54cbb6da7f565889f23681b131a91025dba8e4be
Teller Wetzel <tellerwetzel@yahoo.com>
2021-04-23 19:06:52 -0600

    trying to display data

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,45 +5,81 @@ date.textContent = rightNow;
 
 function myFunction() {
   var searchTerm = document.querySelector("#searchTerm").value;
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=27bc004fc8746779f50a74c093ff9a93"
-)
-.then(function(response) {
-  return response.json();
-})
-.then(function(data) {
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=27bc004fc8746779f50a74c093ff9a93"
+    )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var city = data.name;
+      makeCard(city)
+    console.log(city);
   return fetch(
     "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=27bc004fc8746779f50a74c093ff9a93"
     )
 })
 .then(function(response) {
+  if(response.ok){
   return response.json();
+  }
+  else {
+    alert("Error: " + response.statusText);
+  }
 })
 .then(function(data) {
-  // $("#responseCity").empty();
-  for (var i = 0; i < data.current.length; i++){
-    var temp = data.current[i].temp;
-    var humidity = data.current[i].humidity;
-    var wind = data.current[i].wind_speed;
-    var uv = date.current[i].uvi;
-    // displayCurrent(temp, humidity, wind, uv)
-  }
-  console.log(temp, humidity, wind, uv);
-})
-.catch(function(error) {
-  console.log("No data to display");
-})
-}
-
-function displayCurrent(temp, humidity, wind, uv) {
-  var results = document.getElementById("responseCity");
-  var resultEl = document.createElement("div");
-  resultEl.classList.add("col-6");
-  results.appendChild(resultsEl);
-}
-
-
-
   
+    var temp = data.current.temp;
+    var humidity = data.current.humidity;
+    var uv = data.current.uvi;
+    var wind = data.current.wind_speed;
+    // displayCurrent(temp, humidity, wind, uv)
+  console.log(temp, humidity, uv, wind);
+})
+// .catch(function(error) {
+//   console.log("No data to display");
+// })
+}
+function makeCard(city) {
+var weather = document.getElementById("response-city");
+var cardEl = document.createElement("div");
+cardEl.classList.add("card");
+weather.appendChild(cardEl);
+
+var display = document.createElement("div");
+display.className = "card-body"
+cardEl.appendChild(display);
+
+var cardTitle = document.createElement("h5");
+cardTitle.className = "card-title";
+cardTitle.textContent = + city
+display.appendChild(cardTitle);
+// displayCurrent(temp, humidity, wind, uv)
+
+}
+
+// function displayCurrent(temp, humidity, wind, uv) {
+
+//   var tempInput = document.createElement("p");
+//   tempInput.className = "text";
+//   tempInput.textContent = "Temp: " + temp + "F";
+//   display.appendChild(tempInput);
+  
+//   var humInput = document.createElement("p");
+//   humInput.className = "text";
+//   humInput.textContent = "Humidity: " + humidity + "%";
+//   display.appendChild(humInput);
+
+//   var windInput = document.createElement("p");
+//   windInput.className = "text";
+//   windInput.textContent = "Wind: " + wind + " MPH";
+//   display.appendChild(windInput);
+
+//   var uvInput = document.createElement("p");
+//   uvInput.className = "text";
+//   uvInput.textContent = "UV Index: " + uv;
+//   display.appendChild(uvInput);
+
+// }
